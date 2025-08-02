54b8c989c6ac452fdd87fc55dbc1e422c4d28150
Hritvi Narvekar <hritvinarvekar@outlook.com>
2025-08-02 20:13:36 +0530

    Create cool.cpp

---
+++ cool.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        for(int j=(2*(n-i));j>=1;j--){
+            cout<<" ";
+        }
+        for(int j=i;j>=1;j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}

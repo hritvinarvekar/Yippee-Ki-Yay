b6eecdb316ccf742a3b8a1e893591c761ff704b1
Hritvi Narvekar <hritvinarvekar@outlook.com>
2025-08-02 20:08:24 +0530

    Create confusing.cpp

---
+++ confusing.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            if((i%2==0 && j%2!=0) || (i%2!=0 && j%2==0) ){
+                cout<<0;
+            }
+            else{
+                cout<<1;
+            }
+        }
+        cout<<endl;
+    }
+}

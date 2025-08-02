17750de5ef57720148c2ac0a25161e109b61b51d
Hritvi Narvekar <hritvinarvekar@outlook.com>
2025-08-02 22:08:03 +0530

    Create alphabetPattern.cpp

---
+++ alphabetPattern.cpp
@@ -0,0 +1,25 @@
+
+/* First method
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter: ";
+    cin>>n;
+    char d= 'A'+n;
+    for(int i=1;i<=n;i++){
+        char j='A'+i;
+        for(char s='A'; s<j;s++){
+            cout<<s;
+        }
+        cout<<endl;
+    }
+} 
+*/
+
+/* Second method yet to do */
+
+/*Second pattern 
+
+
